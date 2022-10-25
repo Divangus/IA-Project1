487673766c4b11451ddc02626aeab0088c1c090f
Marina Albalà <70697960+Vizalt@users.noreply.github.com>
2022-10-25 09:14:26 +0200

    script fsm in pilgrim

---
+++ IA-Project1/Assets/allscripts.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class allscripts : MonoBehaviour
+{
+    public GameObject target;
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //void Seek(Vector3 location)
+    //{
+    //    agent = SetDestination(location);
+    //}
+
+    //private NavMeshAgent SetDestination(Vector3 location)
+    //{
+
+    //    return ;
+    //}
+}
